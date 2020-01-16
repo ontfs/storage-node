8be89fec4cdb8806c28ee638a56546e17870f765
rongyi <rongyi@onchain.com>
2020-01-16 10:04:28 +0800

    Update node_start script

--- scripts/node_start.sh
+++ scripts/node_start.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-./ontfs --rpcport=1989 --listenaddr="127.0.0.1:1990" --loglevel=1 --pdpcircuit="./circuit"
+# 1. mappingaddr is elastic IP in cloud
+# 2. chainaddr: ontfs block chain
+# 3. circuit: the circuit file
+./ontfs  --listenaddr="10.0.0.2:1990"  --wallet ./wallet.dat --password server --rpcaddr http://chainaddr:20336  --networkid 7 --maxstorage 450G --loglevel 1 --rpcport 1989 --mappingaddr "8.8.2.2:1990" --pdpcircuit "./circuit"
