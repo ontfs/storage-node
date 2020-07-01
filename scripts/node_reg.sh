4f49607abe1d5ddc7cc582a20287fd58c612cbb2
rongyi <rongyi@onchain.com>
2020-07-01 14:18:06 +0800

    Update script

--- scripts/node_reg.sh
+++ scripts/node_reg.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./ontfs node register --nodenetaddr="127.0.0.1:1990" --volume=10G --servicetime="2020-12-15 12:00:00" --rpcport=1989
+./ontfs node register --nodenetaddr="tcp://127.0.0.1:1990" --volume=10G --servicetime="2020-12-15 12:00:00" --rpcport=1989
